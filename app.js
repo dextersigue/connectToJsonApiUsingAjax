c9ed6ea0fc5486bb2f3e9ead3a6a55f7e1b53d90
Dexter Sigue <dextersigue@gmail.com>
2022-12-26 10:06:43 +0800

    First Commit add files

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const btn = document.querySelector('button');
+const output = document.querySelector('.output');
+const url = 'https://swapi.dev/api/';
+
+btn.addEventListener('click', ()=>{
+
+})
